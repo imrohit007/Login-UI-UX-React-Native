cb8a02dfd12a548310761a586054ae254a7bfe5c
imrohit007 <rohitthakur9830@gmail.com>
2021-08-01 22:20:33 +0530

    Added navigation and Signup

--- App.js
+++ App.js
@@ -1,253 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Input, NativeBaseProvider, Button, Icon, Box, Image, AspectRatio } from 'native-base';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { alignContent, flex, flexDirection, width } from 'styled-system';
+
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
 
 
 function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.Middle}>
-        <Text style={styles.LoginText}>Login</Text>
-      </View>
-      <View style={styles.text2}>
-        <Text>Don't have an account? </Text>
-        <TouchableOpacity><Text style={styles.signupText}> Sign up</Text></TouchableOpacity>
-      </View>
-
-      {/* Username or Email Input Field */}
-      <View style={styles.buttonStyle}>
-        <Text>Email or Username</Text>
-        <View style={styles.emailInput}>
-          <Input
-            InputLeftElement={
-              <Icon
-                as={<FontAwesome5 name="user-secret" />}
-                size="sm"
-                m={2}
-                _light={{
-                  color: "black",
-                }}
-                _dark={{
-                  color: "gray.300",
-                }}
-              />
-            }
-            variant="outline"
-            placeholder="Username"
-            _light={{
-              placeholderTextColor: "blueGray.400",
-            }}
-            _dark={{
-              placeholderTextColor: "blueGray.50",
-            }}
-
-          />
-        </View>
-      </View>
-
-      {/* Password Input Field */}
-      <View style={styles.buttonStyle}>
-        <Text>Password</Text>
-        <View style={styles.emailInput}>
-          <Input
-            InputLeftElement={
-              <Icon
-                as={<FontAwesome5 name="key" />}
-                size="sm"
-                m={2}
-                _light={{
-                  color: "black",
-                }}
-                _dark={{
-                  color: "gray.300",
-                }}
-              />
-            }
-            variant="outline"
-            secureTextEntry={true}
-            placeholder="Password"
-            _light={{
-              placeholderTextColor: "blueGray.400",
-            }}
-            _dark={{
-              placeholderTextColor: "blueGray.50",
-            }}
-          />
-        </View>
-      </View>
-
-      {/* Button */}
-      <View style={styles.buttonStyle}>
-        <Button style={styles.buttonDesign}>
-            LOGIN
-        </Button>
-      </View>
-
-      {/* Line */}
-      <View style={styles.lineStyle}>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
-        <View>
-          <Text style={{width: 50, textAlign: 'center'}}>Or</Text>
-        </View>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
-      </View>
-
-      {/* Box */}
-      <View style={styles.boxStyle}>
-      <Box style={{height:80, width:80}} 
-        shadow={3}
-        _light={{
-          backgroundColor: "gray.50",
-        }}
-        _dark={{
-          backgroundColor: "gray.700",
-        }}
-      >
-        <AspectRatio ratio={1 / 1}>
-          <Image
-            roundedTop="lg"
-            source={{
-              uri: "https://www.transparentpng.com/thumb/google-logo/colorful-google-logo-transparent-clipart-download-u3DWLj.png",
-            }}
-            alt="image"
-          />
-        </AspectRatio>
-      </Box>
-      <Box style={styles.imageStyle}
-        shadow={3}
-        _light={{
-          backgroundColor: "gray.50",
-        }}
-        _dark={{
-          backgroundColor: "gray.700",
-        }}
-      >
-        <AspectRatio ratio={1 / 1}>
-          <Image
-            
-            roundedTop="lg"
-            source={{
-              uri: "https://www.transparentpng.com/thumb/facebook-logo-png/photo-facebook-logo-png-hd-25.png",
-            }}
-            alt="image"
-          />
-        </AspectRatio>
-      </Box>
-      <Box style={styles.imageStyle}
-        shadow={3}
-        _light={{
-          backgroundColor: "gray.50",
-        }}
-        _dark={{
-          backgroundColor: "gray.700",
-        }}
-      >
-        <AspectRatio ratio={1 / 1}>
-          <Image
-            
-            roundedTop="lg"
-            source={{
-              uri: "https://www.transparentpng.com/thumb/twitter/bird-twitter-socialmedia-icons-png-5.png",
-            }}
-            alt="image"
-          />
-        </AspectRatio>
-      </Box>
-      <Box style={styles.imageStyle}
-        shadow={3}
-        _light={{
-          backgroundColor: "gray.50",
-        }}
-        _dark={{
-          backgroundColor: "gray.700",
-        }}
-      >
-        <AspectRatio ratio={1 / 1}>
-          <Image
-            
-            roundedTop="lg"
-            source={{
-              uri: "https://www.transparentpng.com/thumb/apple-logo/RRgURB-apple-logo-clipart-hd.png",
-            }}
-            alt="image"
-          />
-        </AspectRatio>
-      </Box>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator screenOptions={{headerShown: false}} >
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
   );
 }
 
+
 export default () => {
   return (
-    <NativeBaseProvider>
+    <NavigationContainer>
      
         <App />
       
-    </NativeBaseProvider>
+    </NavigationContainer>
   )
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  LoginText: {
-    marginTop:100,
-    fontSize:30,
-    fontWeight:'bold',
-  },
-  Middle:{
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  text2:{
-    flexDirection:'row',
-    justifyContent:'center',
-    paddingTop:5
-  },
-  signupText:{
-    fontWeight:'bold'
-  },
-  emailField:{
-    marginTop:30,
-    marginLeft:15
-  },
-  emailInput:{
-    marginTop:10,
-    marginRight:5
-  },
-  buttonStyle:{
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15
-  },
-  buttonDesign:{
-    backgroundColor:'#026efd'
-  },
-  lineStyle:{
-    flexDirection:'row',
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15,
-    alignItems:'center'
-  },
-  imageStyle:{
-    width:80,
-    height:80,
-    marginLeft:20,
-  },
-  boxStyle:{
-    flexDirection:'row',
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15,
-    justifyContent:'space-around'
-  },
-});
